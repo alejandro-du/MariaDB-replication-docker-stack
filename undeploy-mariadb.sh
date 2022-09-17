3aa204c86e11b2341682ee3e28579a0df81bbf0f
alejandro-du <alejandro.d.a@gmail.com>
2022-09-18 00:56:17 +0300

    Swarm mode undeploy

--- undeploy-mariadb.sh
+++ undeploy-mariadb.sh
@@ -2,4 +2,10 @@ source default.env
 source custom.env
 
 echo Deleting containers and volumes...
-docker compose down --volumes
+
+if [[ $SWARM_MODE -gt 0 ]]
+then
+	docker stack rm mariadb
+else
+	docker compose down --volumes
+fi
